df8ec8f04a69bb5b37d51a1ca5ffe564886e7b2a
Nathan Cheung <nathan.cheung@slu.edu>
2018-12-05 14:58:22 -0600

    Adjusted for when length of sim_data is not equal to n

--- R/RejectionSample.R
+++ R/RejectionSample.R
@@ -3,11 +3,11 @@
 #' @param x A number
 #' @param y A number
 #' @return The sum of \code{x} and \code{y}
-#' @examples n/a
+#' @examples
 #' @export
 
-rejectionsample <- function(n,pdf,a,b,C)
-  sim_data <- replicate(n, {
+rejectionsample <- function(n,pdf,a,b,C){
+  sim_data <- replicate(10000, {
     u <- runif(1, a, b)
     v <- runif(1, 0, C)
     if(v < pdf(u)) {#then accept else reject
@@ -19,10 +19,18 @@ rejectionsample <- function(n,pdf,a,b,C)
 
   sim_data <- sim_data[!is.na(sim_data)]
 
-  if length(sim_data) > n
-    sim_data(1:n) ==
-
+  if (length(sim_data) >= n) {
+    sim_data <- sim_data[1:n]
+  }else if (length(sim_data) < n){
+    while (length(sim_data) < n)
+      u <- runif(1, a, b)
+      v <- runif(1, 0, C)
+      if (v < pdf(u))
+        sim_data = c(sim_data,u)
+      }
+  }
 
 
 hist(sim_data, probability = TRUE)
 curve(pdf, add = TRUE)
+
